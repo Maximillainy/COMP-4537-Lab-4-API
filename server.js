deedfa070ee073a15eda8521de2bb32cb7c8f359
Maximilliany <maxillian4@gmail.com>
2024-02-14 17:08:35 -0800

    Refactored endpoint check

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const http = require('http');
 const url = require('url');
 const functions = require('./modules/utils');
+
 const endPoint = "/api/definitions/"
 let numRequests = 0;
 let dictionary = {};
 
 http.createServer(function (req, res) {
+    const reqPath = url.parse(req.url, true).pathname;
+    numRequests++;
+
     res.writeHead(200, {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST',
     });
-    
+
     /**
      * Client Request:
      * GET /api/definitions/?word=boo
@@ -22,8 +26,7 @@ http.createServer(function (req, res) {
      * "definition": "A written or printed work consisting of pages glued or sewn together"
      * }
      */
-    if (req.method === "GET") {
-        numRequests++;
+    if (req.method === "GET" && reqPath === endPoint) {
         const query = url.parse(req.url, true).query;
         const word = query.word;
         if (functions.validateInput(word)) {
@@ -35,7 +38,7 @@ http.createServer(function (req, res) {
                 res.write(JSON.stringify({ request: numRequests, definition: "Word not found!" }));
             }
         } else {
-            res.writeHead(400, { 'Content-Type': 'apllication/json' });
+            res.writeHead(400, { 'Content-Type': 'application/json' });
             res.write(JSON.stringify({ request: numRequests, definition: "Invalid input!" }));
         }
         res.end();
@@ -48,8 +51,7 @@ http.createServer(function (req, res) {
      * Save entry to array in memory, no persistent storage
      * If a definition exists, respond: "Definition already exists!"
      */
-    if (req.method === 'POST' && req.url === endPoint) {
-        numRequests++;
+    if (req.method === 'POST' && reqPath === endPoint) {
         let body = '';
          req.on('data', function (chunk) {
             if (chunk != null) {
@@ -66,11 +68,11 @@ http.createServer(function (req, res) {
             if (functions.validateInput(word) && functions.validateInput(definition)) {
                 if (dictionary[word]) {
                     res.writeHead(400, { 'Content-Type': 'application/json' });
-                    res.write(JSON.stringify({ request: numRequests, response: "Definition already exists!" }));
+                    res.write(JSON.stringify({ request: numRequests, response: "Word already exists!" }));
                 } else {
                     dictionary[word] = definition;
                     res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.write(JSON.stringify({ request: numRequests, response: "Seccessfully added word: " + word}));
+                    res.write(JSON.stringify({ request: numRequests, response: "Successfully added word: " + word}));
                 }
             } else {
                 res.writeHead(400, { 'Content-Type': 'application/json' });
